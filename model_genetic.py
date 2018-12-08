48c0accdde10d265b779eff8113e949f84265c58
Hiren <hiren@Hirens-Mac-mini-2.local>
2018-12-08 17:24:08 +0530

    keeping up

--- model_genetic.py
+++ model_genetic.py
@@ -330,7 +330,7 @@ class DCGAN(object):
               }
             )
             save_images(samples, image_manifold_size(samples.shape[0]),
-                  '{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
+                  '{}/train_{:02d}_{:04d}_{}_{}.png'.format(config.sample_dir, epoch, idx, d_loss, g_loss))
             print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss)) 
           else:
             try:
@@ -342,7 +342,7 @@ class DCGAN(object):
                 },
               )
               save_images(samples, image_manifold_size(samples.shape[0]),
-                    '{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
+                    '{}/train_{:02d}_{:04d}_{}_{}.png'.format(config.sample_dir, epoch, idx, d_loss, g_loss))
               print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss)) 
             except:
               print("one pic error!...")
